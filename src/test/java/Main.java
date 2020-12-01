b8c2e2255e6b082d371034413422d6b4c67d3f0e
gpw <gpw18636495564@tom.com>
2020-12-01 22:13:58 +0800

    "琪学APP的模板 第二次提交"

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("第一次提交");
-=======
+
         System.out.println("第一个程序开始");
->>>>>>> origin/master
+
+        System.out.println("第二次提交");
     }
 }
