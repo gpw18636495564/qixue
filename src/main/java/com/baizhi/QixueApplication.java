403fb7d7212c8d026115ed4b6183129fff42f8a7
gpw <gpw18636495564@tom.com>
2020-12-01 21:38:27 +0800

    "琪学APP的模板"

---
+++ src/main/java/com/baizhi/QixueApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QixueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QixueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baizhi/qixue/QixueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi.qixue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QixueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
